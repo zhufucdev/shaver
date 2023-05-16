dbda71d03c63f37b6373caeb82dfa760796cd87e
zhufucdev <1095291434@qq.com>
2023-05-16 21:47:52 +0800

    Added epic sound effect

--- src/main/java/com/zhufucdev/shaver/client/ShaverClient.kt
+++ src/main/java/com/zhufucdev/shaver/client/ShaverClient.kt
@@ -2,6 +2,7 @@ package com.zhufucdev.shaver.client
 
 import com.zhufucdev.shaver.DISPLAY_BUILT_ID
 import com.zhufucdev.shaver.RESULT_SHOWN_SHEEP_SHEARED_ID
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking
 import net.minecraft.particle.ParticleTypes
 import net.minecraft.sound.SoundCategory
@@ -10,14 +11,33 @@ import net.minecraft.sound.SoundEvents
 fun init() {
     ClientPlayNetworking.registerGlobalReceiver(DISPLAY_BUILT_ID) { client, _, buf, _ ->
         val pos = buf.readBlockPos().toCenterPos()
-        client.world?.addParticle(ParticleTypes.EXPLOSION, pos.x, pos.y, pos.z, 1.0, 1.0, 1.0)
+        client.execute {
+            client.world?.addParticle(ParticleTypes.EXPLOSION, pos.x, pos.y, pos.z, 1.0, 1.0, 1.0)
+        }
     }
     ClientPlayNetworking.registerGlobalReceiver(RESULT_SHOWN_SHEEP_SHEARED_ID) { client, _, _, _ ->
-        client.player?.playSound(
-            SoundEvents.ENTITY_SHEEP_SHEAR,
-            SoundCategory.AMBIENT,
-            10F,
-            1F
-        )
+        client.execute {
+            client.player?.playSound(
+                SoundEvents.ENTITY_SHEEP_SHEAR,
+                SoundCategory.AMBIENT,
+                10F,
+                1F
+            )
+        }
+        var timer = 0
+        ClientTickEvents.START_CLIENT_TICK.register {
+            if (timer < 100) {
+                timer++
+            } else if (timer == 100) {
+                timer++
+                client.player?.playSound(
+                    SoundEvents.UI_TOAST_CHALLENGE_COMPLETE,
+                    10F,
+                    1F
+                )
+            }
+        }
+        client.execute {
+        }
     }
 }
